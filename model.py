2577a4a7f809f3c1685088dceb3b9154267d7def
Fausto De La Rosa Mañón <56518301+fmanon@users.noreply.github.com>
2021-06-25 18:09:07 -0400

    Update model.py

---
+++ model.py
@@ -0,0 +1,17 @@
+# A python program to illustrate Caesar Cipher Technique
+def encrypt(text,s):
+    result = ""
+ 
+    # traverse text
+    for i in range(len(text)):
+        char = text[i]
+ 
+        # Encrypt uppercase characters
+        if (char.isupper()):
+            result += chr((ord(char) + int(s)-65) % 26 + 65)
+ 
+        # Encrypt lowercase characters
+        else:
+            result += chr((ord(char) + int(s) - 97) % 26 + 97)
+ 
+    return result
