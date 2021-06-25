7ed171d9569c4c217949f5952725891f1f9d7d9b
Fausto De La Rosa Mañón <56518301+fmanon@users.noreply.github.com>
2021-06-25 18:08:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import encrypt
 
 
 # -- Initialization section --
@@ -13,4 +14,14 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
+    if request.method=='POST':
+        shift = request.form['shift']
+        original_message = request.form['message']
+        encryption = encrypt(original_message, shift)
+        return render_template("result.html", shift = shift, original_message = original_message, encryption = encryption)
+    else:
+        return "<h2>dique '404 error'...</h2>"
